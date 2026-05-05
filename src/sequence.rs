4372d7f43ca73231090758a1f7eb6f1f01f76491
Tibet Tornaci <oofdere@hey.com>
2026-05-04 23:48:11 -0700

    add some methods to sequence (they're not very good i just want to checkpoint)

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,3 +1,4 @@
+use crate::{Batch, Context, LlamaSampler, Model};
 use llama_sys::*;
 use std::{
     cell::RefCell,
@@ -7,7 +8,6 @@ use std::{
     vec,
 };
 
-use crate::{Context, LlamaSampler, Model};
 
 pub struct Sequence<'ctx, 'a> {
     ctx: &'a Context<'ctx>,
@@ -17,6 +17,45 @@ pub struct Sequence<'ctx, 'a> {
 impl<'ctx, 'a> Sequence<'ctx, 'a> {
     pub(crate) fn new(ctx: &'a Context<'ctx>, id: llama_seq_id) -> Self {
         Self { ctx, id }
+    }
+
+    /// Removes all tokens that belong to the specified sequence and have positions in the provided range.
+    /// 
+    /// Returns false if a partial sequence cannot be removed. Removing a whole sequence never fails.
+    /// 
+    /// - if start is negative it will be treated as `0`
+    /// - if end is negative it will be treated as the end of the sequence
+    pub fn remove(&mut self, range: Range<usize>) -> bool {
+        if self.kv_remove(range.start as i32..range.end as i32) {
+            self.tokens().borrow_mut().drain(range);
+            true
+        } else {
+            false
+        }
+    }
+
+    /// overwrites the other sequence with the tokens in this sequence
+    pub fn copy_to(&self, other: &mut Self, range: Range<usize>) {
+        self.kv_copy(other, range.start as i32..range.end as i32);
+        let mut tokens = other.tokens().borrow_mut();
+        tokens.clear();
+        tokens.extend_from_slice(&self.tokens().borrow()[range.start..range.end]);
+    }
+
+    /// overwrites the tokens in this sequence with the tokens from the other sequence
+    pub fn copy_from(&mut self, other: &Self, range: Range<usize>) {
+        other.copy_to(self, range);
+    }
+
+    /// Adds relative position "delta" to all tokens that belong to the specified sequence and have positions in [p0, p1)
+    /// p0 < 0 : [0,  p1]
+    /// p1 < 0 : [p0, inf)
+    fn shift(&mut self, range: Range<llama_pos>, delta: llama_pos) {
+        if self.ctx.can_shift() {
+            unsafe { llama_memory_seq_add(self.ctx.get_memory(), self.id as i32, range.start, range.end, delta) }
+        } else {
+            todo!("add fallback for when seq_add is not supported")
+        }
     }
 
     /// Returns the smallest position present in the memory for the specified sequence.
@@ -72,7 +111,7 @@ impl<'ctx, 'a> Sequence<'ctx, 'a> {
     /// Adds relative position "delta" to all tokens that belong to the specified sequence and have positions in [p0, p1)
     /// - if start is negative it will be treated as `0`
     /// - if end is negative it will be treated as the end of the sequence
-    pub fn kv_add(&mut self, range: Range<llama_pos>, delta: llama_pos) {
+    pub fn kv_shift(&mut self, range: Range<llama_pos>, delta: llama_pos) {
         unsafe {
             llama_memory_seq_add(
                 self.ctx.get_memory(),
