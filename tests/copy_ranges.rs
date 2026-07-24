358d1f7fd5bac540687242af8fe3e3445e1290c7
oofdere <oofdere@hey.com>
2026-07-23 18:59:44 -0700

    test copy range validation

---
+++ tests/copy_ranges.rs
@@ -0,0 +1,30 @@
+mod common;
+
+use rusty_llama::Context;
+
+fn context_with_copy_support() -> Context {
+    let (model, _) = common::load_model_and_context();
+    let mut params = common::test_ctx_params();
+    params.kv_unified = true;
+    Context::new(&model, &params).unwrap()
+}
+
+#[test]
+#[should_panic(expected = "sequence range out of bounds")]
+fn copy_to_rejects_out_of_bounds_range() {
+    let ctx = context_with_copy_support();
+    let src = ctx.sequence().unwrap();
+    let mut dst = ctx.sequence().unwrap();
+
+    src.copy_to(&mut dst, 0..1);
+}
+
+#[test]
+#[should_panic(expected = "sequence range out of bounds")]
+fn copy_to_async_rejects_out_of_bounds_range() {
+    let ctx = context_with_copy_support();
+    let src = ctx.sequence().unwrap();
+    let mut dst = ctx.sequence().unwrap();
+
+    pollster::block_on(src.copy_to_async(&mut dst, 0..1));
+}
